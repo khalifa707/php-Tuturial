bd235dd263b25b77201e756a7ff373439be7e8bd
Khalifa <bodakhalifa707@gmail.com>
2025-02-15 16:26:37 +0200

    Working with Functions

--- Control structure/if statement.php
+++ Control structure/if statement.php
@@ -1,10 +1,17 @@
 <?php
 
-$number = 10;
+/*$number = 10;
 $number2=20;
 if($number > $number2){
     echo "It's Not";
 }
 else{
     echo "It's";
+}*/
+
+$x="2";
+$y=2;
+
+if ($x==$y){
+    echo "It's true";
 }

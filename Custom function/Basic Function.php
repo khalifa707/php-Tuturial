22e8ca0b538d8b78d8c1203021016008cbd6bc2d
Khalifa <bodakhalifa707@gmail.com>
2025-02-15 16:44:17 +0200

    Finding the even number

--- Custom function/Basic Function.php
+++ Custom function/Basic Function.php
@@ -10,10 +10,27 @@ greet();*/
     echo $length * $width;
 }
 calculateArea(10,20);*/
-function calculateArea($length,$width)
+/*function calculateArea($length,$width)
 {
     return $length * $width;
 }
 
 $area = calculateArea(2,20);
-echo 50 + $area;
+echo 50 + $area;*/
+
+function isEven($number) {
+    // Check if the number is divisible by 2
+    if ($number % 2 == 0) {
+        return true; // The number is even
+    } else {
+        return false; // The number is odd
+    }
+}
+
+// Example usage:
+$number = 11;
+if (isEven($number)) {
+    echo "$number is even.";
+} else {
+    echo "$number is odd.";
+}

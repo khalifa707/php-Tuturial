807a7193defc0f149a1413cc5c97b5590408e625
Khalifa <bodakhalifa707@gmail.com>
2025-02-15 16:34:21 +0200

    Working with Return Functions

--- Custom function/Basic Function.php
+++ Custom function/Basic Function.php
@@ -5,8 +5,15 @@
 }
 greet();*/
 
-function calculateArea($length,$width)
+/*function calculateArea($length,$width)
 {
     echo $length * $width;
 }
-calculateArea(10,20);
+calculateArea(10,20);*/
+function calculateArea($length,$width)
+{
+    return $length * $width;
+}
+
+$area = calculateArea(2,20);
+echo 50 + $area;

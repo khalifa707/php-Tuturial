faa2d5898f9a946a4e7c1d0fb049feb430b1c1d2
Khalifa <bodakhalifa707@gmail.com>
2025-02-16 14:13:23 +0200

    working with loops (while do)

---
+++ Loops/while do.php
@@ -0,0 +1,12 @@
+<?php
+
+/*$index= 1;
+while ($index < 10) {
+    echo "Number: " . $index . "<br>";
+    $index++;
+}*/
+$index = 1;
+do{
+    echo "Number: " . $index . "<br>";
+    $index++;
+}while($index < 10);

aec11e8838b24b7e81fe36c5a82d1278c9ec982c
Khalifa <bodakhalifa707@gmail.com>
2025-02-16 14:39:05 +0200

    working with loops (for each)

---
+++ Loops/break and continue.php
@@ -0,0 +1,14 @@
+<?php
+
+/*for ($i = 1; $i <= 10; $i++) {
+    if ($i ==3) {
+        break;
+    }
+    echo "Number: " . $i . "<br>";
+}*/
+for ($i = 1; $i <= 10; $i++) {
+    if ($i ==3) {
+        continue;
+    }
+    echo "Number: " . $i . "<br>";
+}

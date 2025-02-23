0332fa1897b9834cb9ec678bb9d296e468febfd1
Khalifa <bodakhalifa707@gmail.com>
2025-02-24 01:03:41 +0200

    working with Arrays (Superglobals)

---
+++ Arrays/Superglobals.php
@@ -0,0 +1,14 @@
+<?php
+/*$app = "php CMS";
+
+function example()
+{
+echo $GLOBALS["app"];
+}
+example();*/
+
+$name = $_GET["name"];
+$age = $_GET["age"];
+
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";

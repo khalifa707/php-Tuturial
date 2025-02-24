4344d8b881e5c4b78f37eedc0deb3a768cb7397b
Khalifa <bodakhalifa707@gmail.com>
2025-02-24 02:05:53 +0200

    working with Builtin Function (Math Function)

---
+++ Built In Functions/Math Function.php
@@ -0,0 +1,15 @@
+<?php
+/*
+echo abs(-20) . "<br>";
+echo round(19.55255) . "<br>";
+echo ceil(19.55255) . "<br>";
+echo floor(19.55255) . "<br>";
+
+$numbers = [1,2,3,4,5,6,7,8,9];
+echo max($numbers) . "<br>";
+echo min($numbers) . "<br>";
+
+$base=2;
+$expo=3;
+
+echo pow($base, $expo) . "<br>";*/

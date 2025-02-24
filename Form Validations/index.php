def2fec0230e6c7dcccd6fcdc478a5931dd9dbb4
Khalifa <bodakhalifa707@gmail.com>
2025-02-24 04:03:02 +0200

    working with Form Functions (Basic Form)

---
+++ Form Validations/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple Form</title>
+</head>
+<body>
+
+<h2>Simple Form</h2>
+
+<form action="Process.php">
+    <input name="Username" type="text" placeholder="Enter Username" aria-label="">
+    <input name="Password" type="text" placeholder="Enter Password" aria-label="">
+    <input type="Submit" aria-label="">
+</form>
+
+</body>
+</html>
